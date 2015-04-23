4ed60ed0424ca1935481c9b01e08c3320f33c30f
Giskard Solutions <kamil4521@gmail.com>
2015-04-23 17:08:34 +0200

    Added GendarmeSetup & RuleConfigGenerator

---
+++ GendarmeNUnit/GendarmeSetup.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GiskardSolutions.GendarmeNUnit
+{
+    [SetUpFixture]
+    public abstract class GendarmeSetup
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Initialize();
+            RunGendarme();
+        }
+
+        protected abstract void Initialize();
+
+        private void RunGendarme()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GendarmeNUnit/RuleConfigGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace GiskardSolutions.GendarmeNUnit
+{
+    public abstract class RuleConfigGenerator
+    {
+        public class CustomConfig : RuleConfigGenerator
+        {
+            public CustomConfig() : base()
+            {
+                _ruleLibaryCollection = new List<string>();
+            }
+
+            public void AddRuleLibrary(string ruleLibraryPath)
+            {
+                if (!File.Exists(ruleLibraryPath))
+                    throw new Exceptions.RuleLibraryFileNotExists(ruleLibraryPath);
+
+                _ruleLibaryCollection.Add(ruleLibraryPath);
+            }
+
+            protected override void UpdateXml()
+            {
+                base.UpdateXml();
+            }
+
+            private readonly List<string> _ruleLibaryCollection;
+        }
+
+        public void Save(string fileName)
+        {
+            UpdateXml();
+            RuleConfig.Save(fileName);
+        }
+
+        protected XElement RuleConfig { get; private set; }
+
+        protected RuleConfigGenerator()
+        {
+            RuleConfig = XElement.Load("exampleRules.xml");
+        }
+
+        protected virtual void UpdateXml()
+        {
+        }
+
+        public class Exceptions : Exception
+        {
+            public class RuleLibraryFileNotExists : Exceptions
+            {
+                public RuleLibraryFileNotExists(string file) : base(string.Format("Rule library {0} not exists", file)) { }
+            }
+
+            public Exceptions() : base() { }
+            public Exceptions(string message) : base(message) { }
+        }
+    }
+}
