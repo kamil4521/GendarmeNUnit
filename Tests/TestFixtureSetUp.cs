03da34b06e81bdd0b85930a7a34cfa99eb9dd80e
Giskard Solutions <kamil4521@gmail.com>
2015-04-23 18:24:45 +0200

    First experimental unit tests

---
+++ Tests/TestFixtureSetUp.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public abstract class TestFixtureSetUp
+    {
+        [SetUp]
+        public virtual void Setup() { }
+    }
+}
